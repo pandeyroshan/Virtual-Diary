68cb2a7cddc3fa9f51e219d22251fa2ea65f494e
= <=>
2019-04-16 22:08:11 +0530

    Final Commit

--- entries/forms.py
+++ entries/forms.py
@@ -6,7 +6,6 @@ class EntryForm(ModelForm):
     class Meta:
         model = Entry
         fields = ('text',)
-    
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.fields['text'].widget.attrs.update({'class': 'textarea', 'placeholder': 'What\'s in your mind'})
--- entries/views.py
+++ entries/views.py
@@ -5,13 +5,13 @@ from .forms import EntryForm
 
 def index(request):
     entries = Entry.objects.order_by('-date_posted')
+    ##Entry.objects.all().delete()  ## UnComment it to flash the database
     context = {'entries' : entries}
     return render(request,'entries/index.html',context)
 
 def add(request):
     if request.method == 'POST':
         form = EntryForm(request.POST)
-
         if form.is_valid():
             form.save()
             return  redirect('index')
@@ -19,3 +19,9 @@ def add(request):
         form = EntryForm()
     context = { 'form' : form }
     return render(request,'entries/add.html',context)
+
+def reset(request):
+    Entry.objects.all().delete()
+    entries = Entry.objects.order_by('-date_posted')
+    context = { 'entries' : entries}
+    return render(request,'entries/index.html',context)
